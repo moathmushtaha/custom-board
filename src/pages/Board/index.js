0cfa60c25fc1c9c9ee3341d596d134f615c3377b
Moaz A.Moshtha <moazmoshtha@gmail.com>
2022-09-28 19:11:06 +0300

    refactore and add ItemsProvider

--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import SlidingPane from "react-sliding-pane";
 import Close from "monday-ui-react-core/dist/icons/Close";
 import {Button} from "monday-ui-react-core";
@@ -6,64 +6,15 @@ import {Button} from "monday-ui-react-core";
 import {ItemForm} from "../../components/ItemForm";
 import ItemsTable from "../../components/ItemsTable";
 import {useMonday} from "../../contexts/MondayContext";
-import {useMutation, gql} from '@apollo/client';
+import {ItemsProvider} from "../../contexts/ItemsContext";
 
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import './style.css';
 
-const ADD_ITEM = gql`
-    mutation CreateItem($name: String!, $description: String!, $status: String!) {
-        createItem(itemInput: {name: $name, description: $description, status: $status}) {
-            _id
-            name
-        }
-    }
-`;
-
-const UPDATE_ITEM = gql`
-    mutation CreateItem($_id:ID!,$name: String!, $description: String!, $status: String!) {
-        updateItem(_id:$_id,itemInput:{
-            name: $name,
-            description: $description,
-            status: $status
-        }){
-            name
-        }
-    }
-`;
-
 const Board = () => {
     const [isPaneOpen, setIsPaneOpen] = useState(false);
-    const [addItemMutateFunction, {addData, addLoading, addError}] = useMutation(ADD_ITEM);
-    const [updateItemMutateFunction, {updateData, updateLoading, updateError}] = useMutation(UPDATE_ITEM);
     const [isAdd, setIsAdd] = useState(true);
-    const [item, setItem] = useState({_id:'',name: '', description: '', status: ''});
-
-    if (addError || updateError) {
-        alert('Error');
-    }
-
-    const onSubmit = (values) => {
-        if (isAdd) {
-            addItemMutateFunction({
-                variables: {
-                    name: values.name,
-                    description: values.description,
-                    status: values.status
-                }
-            }).then(() => togglePane());;
-        } else {
-            console.log('update',item._id);
-            updateItemMutateFunction({
-                variables: {
-                    _id: item._id,
-                    name: values.name,
-                    description: values.description,
-                    status: values.status
-                }
-            }).then(() => togglePane());;
-        }
-    };
+    const [item, setItem] = useState({_id: '', name: '', description: '', status: ''});
 
     const togglePane = () => {
         resetItem();
@@ -72,7 +23,7 @@ const Board = () => {
     };
 
     const resetItem = () => {
-        setItem({_id:'',name: '', description: '', status: ''});
+        setItem({_id: '', name: '', description: '', status: ''});
     }
 
     const onRowClick = (row) => {
@@ -83,18 +34,20 @@ const Board = () => {
 
     return (
         <div className="container mt-5">
-            <Button className="mb-3" size="small" onClick={togglePane}>New Item</Button>
-            <SlidingPane
-                width="30%"
-                isOpen={isPaneOpen}
-                title="Item View"
-                onRequestClose={togglePane}
-                from="right"
-                closeIcon={<Close/>}
-            >
-                <ItemForm onSubmit={onSubmit} item={item} isAdd={isAdd}/>
-            </SlidingPane>
-            <ItemsTable onRowClick={onRowClick}/>
+            <ItemsProvider>
+                <Button className="mb-3" size="small" onClick={togglePane}>New Item</Button>
+                <SlidingPane
+                    width="30%"
+                    isOpen={isPaneOpen}
+                    title="Item View"
+                    onRequestClose={togglePane}
+                    from="right"
+                    closeIcon={<Close/>}
+                >
+                    <ItemForm togglePane={togglePane} item={item} isAdd={isAdd}/>
+                </SlidingPane>
+                <ItemsTable onRowClick={onRowClick}/>
+            </ItemsProvider>
         </div>
     )
 }
